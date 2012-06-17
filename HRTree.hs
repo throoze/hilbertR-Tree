14a9124d0d8584b6ec80108ed3350d22f58c5e8c
Victor Manuel De Ponte Olivares <rdbvictor19@gmail.com>
2012-06-17 02:15:28 -0430

    'delete' function done. Not tested. Need to complete some auxiliar functions.
    The 'remove' function (TO DO) does the deletion of the rectangle passed and propagates the underflow if necessary.

--- HRTree.hs
+++ HRTree.hs
@@ -28,6 +28,7 @@ orden entre los mismos.
 
 --        ) where
 
+-- ESTO ES UN EJEMPLO DE DOCUMENTACIÓN EN HADDOC
 -- -- | Función 'dequeue'. Desencola el siguiente elemento en la cola de
 -- --   prioridades, y devuelve una tupla con dicho elemento como primera
 -- --   componente, y la /PQueue/ sin el elemento como segunda componente.
@@ -50,12 +51,19 @@ type Point = ( Int, Int )
 
 data RTree = RTree Int Int HRTree deriving (Show)
 
---data NodeInfo = NI {mbr :: MBR, tree :: HRTree, lhv :: LHV}
---data LeafInfo = LI {rec :: Rectangle, hv :: LHV}
---              deriving(Show)
-data HRTree = Node {tree::(Seq HRTree), mbr :: MBR, lhv :: LHV} 
-            | Leaf {recs::(Seq Rectangle), lmbr :: MBR, llhv :: LHV} 
+data HRTree = Node {tree::(Seq HRTree), mbr :: MBR, lhv :: LHV}
+            | Leaf {recs::(Seq Rectangle), lmbr :: MBR, llhv :: LHV}
             deriving(Show)
+
+data Crumb = Crumb MBR LHV (Seq HRTree) (Seq HRTree)
+
+type Zipper = ( HRTree , (Seq Crumb) )
+
+instance Ord Rectangle where
+  r1 < r2 = hilbert r1 < hilbert r2
+
+emptyRectangle :: Rectangle
+emptyRectangle = R (0,0) (0,0) (0,0) (0,0)
 
 isRectangle :: Rectangle -> Bool
 isRectangle r = snd (ul r) > snd (ll r)
@@ -97,64 +105,81 @@ hilbertDistance d (x,y)
 hilbert :: Rectangle -> Int
 hilbert r = hilbertDistance 65536 $ centroid r
 
-instance Ord Rectangle where
-  r1 < r2 = hilbert r1 < hilbert r2
-
 {-Api del RTree-}
 
+data RTreeError = RepeatedRectangleInsertion Rectangle
+                | NonExistentRectangleDeletion Rectangle
+                | UnknownError String
+
+instance Error RTreeError where
+  noMsg = UnknownError "unknown error"
+  strMsg = UnknownError
+
+instance Show RTreeError where
+  show (RepeatedRectangleInsertion r) =
+    "Repeated rectangle insertion\n\n" ++ (show r) ++ "\n\n"
+  show (NonExistentRectangleDeletion r) =
+    "Non-Existent rectangle deletion\n\n" ++ (show r) ++ "\n\n"
+  show (UnknownError msg) = msg
+
 newRTree :: Int -> Int -> RTree
-newRTree cl cn = RTree cl cn $ Leaf empty
+newRTree cl cn = RTree cl cn newHRTree
 
+newHRTree :: HRTree
+newHRTree = Leaf empty emptyRectangle 0
 
-search :: RTree -> Rectangle -> Maybe [ Rectangle ]
-search (RTree cl cn t) r = case (toList $ auxSearch r t) of
-  ls@(x:_) -> Just ls
-  []       -> Nothing
+getL :: ViewL a -> a
+getL (x :< xs) = x
+
+head' :: Seq a -> a
+head' sec = getL $ viewl sec
+
+tail' :: Seq a -> Seq a
+tail' sec = case (viewl sec) of
+  (x :< xs) -> xs
+
+{- Some other useful functions on sequences -}
+-- init' :: Seq a -> Seq a
+-- init' sec = case (viewr sec) of
+--   (xs :> x) -> xs
+
+-- last' :: Seq a -> a
+-- last' sec = getR $ viewr sec
+
+-- getR :: ViewR a -> a
+-- getR (xs :> x) = x
+
+deleteFromSeq :: (Eq a) => Seq a -> a -> Seq a
+deleteFromSeq sec elem = foldl' decide empty sec
   where
-    auxSearch :: Rectangle -> HRTree -> Seq Rectangle
-    auxSearch r n@(Node _ _ _) = fromList $ F.concatMap
-                                 (\ ni -> toList $ auxSearch r (tree ni)) $
-                                 S.filter verifyOverlap n
-    auxSearch r l@(Leaf _ _) = S.filter (overlapped r) (recs l)
-    verifyOverlap ni = overlapped r (mbr ni)
+    decide acc e
+      | elem /= e = acc |> e
+      | otherwise = acc
 
---insert :: RTree -> Rectangle -> Either String (Maybe HRTree,HRTree)
---insert (RTree _cl _cn t) r = insert' t r
--- 
---insert' :: HRTree -> Rectangle -> Either String (Maybe HRTree,HRTree)
---insert' (Leaf rs m h) rect = Right $ (Nothing,Leaf ((LI rect 1) <| sons)) --fix
---insert' (Node sons m h) rect = do
---  (i,node) <- Right $ pickNode sons rect
---  (ov,newnode) <- insert' node rect
---  newsons <- Right $ S.update i newnode sons--missing mrb&lhv update
---  Right $ handleOverFlow (Node newsons m h) ov
--- 
---handleOverFlow :: HRTree -> Maybe HRTree -> (Maybe HRTree, HRTree)
---handleOverFlow t Nothing = (Nothing,t)
---handleOverFlow parent@(Node sons m h) (Just ov) = 
---  where (i,cs) = getCooperatingSibling sons ov 
---        (ov,newparent) = either
---                         (split parent)
---                         ((,) Nothing)
---                         insertNodeIfNotFull parent cs ov
---  
---insertNodeIfNotFull :: HRTree -> HRTree -> HRTree -> Either HRTree HRTree
---insertNodeIfNotFull parent n@(Node sons m h) ov =
---  if (S.length sons) < 2 then
---    Right $ newsons m h
---      where newsons = S.unstableSort (ov <| sons)
---  else
---    Left r
--- 
---split
-                                  
---insertRectIfNotFull :: HRTree -> Rectangle -> Either HRTree HRTree
---insertRectIfNotFull (Leaf sons) r = if (S.length sons) < 2 then
---                                      Right $ S.unstableSort (r <| sons)
---                                    else
---                                      Left r
+reconstruct :: Zipper -> HRTree
+reconstruct z@( focus , crumbs )
+  | S.null crumbs = focus
+  | otherwise   = case (head' crumbs) of
+    Crumb mbr lhv before after ->
+      reconstruct (Node (before >< (focus <| after)) mbr lhv , tail' crumbs)
 
-pickNode:: Seq NodeInfo -> Rectangle -> (Int,NodeInfo)
-pickNode sons r = (1,S.index sons 1)
+remove:: Zipper -> Rectangle -> Zipper
+remove z@(Leaf recs mbr lhv, crumbs) r = (newHRTree, empty)
+--remove z@(Leaf recs mbr lhv, crumbs) r =
 
---delete :: tree -> Rectangle -> Either e tree
+delete :: RTree -> Rectangle -> Either e RTree
+delete (RTree cl cn t) r = case (delete' (t,empty) r) of
+  Left e -> Left $ error (show e)
+  Right t -> return $ RTree cl cn t
+  where
+    delete' :: Zipper -> Rectangle -> Either RTreeError HRTree
+    delete' (Node trees mbr lhv, crumbs) r = delete' ( focus , trail ) r
+      where
+        focus = head' afterChosen
+        trail = (Crumb mbr lhv beforeChosen $ tail' afterChosen) <| crumbs
+        beforeChosen = fst $ chooseChild trees
+        afterChosen = snd $ chooseChild trees
+        chooseChild = spanl (\ (Node _ _ lhv) -> lhv < hilbert r)
+    delete' z@(Leaf recs mbr lhv, crumbs) r = case (elemIndexL r recs) of
+      Nothing -> throwError $ NonExistentRectangleDeletion r
+      Just i  -> return $ reconstruct $ remove z r
